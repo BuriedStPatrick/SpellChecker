b6ee53623afed1fb1767b70adc3c2c306589bced
Patrick Christensen <patrick.christensen@eksponent.com>
2015-06-03 20:05:52 +0200

    Added watch functionality

--- app/components/home/homeController.js
+++ app/components/home/homeController.js
@@ -2,4 +2,11 @@ app.controller('HomeController', ['$scope', function($scope){
 	$scope.title = 'The Grammar Nazi';
 	$scope.subheading = 'Look professional like a real Nazi!';
 	$scope.grammar = new Grammar($scope.input, $scope.output);
+	$scope.$watch(function(scope){return scope.grammar.input;}, function(newValue, oldValue){
+		checkGrammar($scope, newValue, oldValue);
+	});
 }]);
+
+function checkGrammar(scope, newValue, oldValue){
+	scope.grammar.output = 'edited: ' + newValue;
+}
